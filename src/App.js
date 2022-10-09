37cf8f8dc8615a3fd37e0fbcffc7800a39d2de77
Mariana Machohan <debrisofme9@gmail.com>
2022-10-09 16:21:20 +0200

    html base

--- src/App.js
+++ src/App.js
@@ -2,9 +2,21 @@
 import './App.css';
 
 function App() {
-  return (
 
+
+  return (
+		<>
+			<div className='App'>
+				<div>
+					<h2>Counter</h2>
+					<h1></h1>
+					<button className='Minus'>- Minus</button>
+					<button className='Plus'>+ Plus</button>
+				</div>
+			</div>
+		</>
   );
+
 }
 
 export default App;
