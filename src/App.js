95b4abe84d367de11f82b4c1f413c081ec345061
Mariana Machohan <debrisofme9@gmail.com>
2022-10-09 16:33:47 +0200

    + count (onPlus)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 
 import './App.css';
 
+
 function App() {
 
+	let count = 0;
+
+	const onClickPlus = () => {
+		count++;
+		console.log(count);
+	}
 
   return (
 		<>
 			<div className='App'>
 				<div>
 					<h2>Counter</h2>
-					<h1></h1>
+					<h1>{count}</h1>
 					<button className='Minus'>- Minus</button>
-					<button className='Plus'>+ Plus</button>
+					<button onClick={onClickPlus} className='Plus'>+ Plus</button>
 				</div>
 			</div>
 		</>
