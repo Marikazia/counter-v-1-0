e627cd27bd7b93df577c76bd7f64323f7b012393
Mariana Machohan <debrisofme9@gmail.com>
2022-10-09 18:29:48 +0200

    1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
-import './index.scss';
 import React from 'react'; 
+import './index.css';
 
 function App() {
 	const [count, setCount] = React.useState(0);
@@ -10,11 +10,11 @@ function App() {
 		setCount(count + 1);
 		// count++;
 		// console.log(count);
-	}
+	};
 
 	const onClickMinus = () => {
 		setCount(count - 1);
-	}
+	};
 
   return (
 		<>
@@ -22,12 +22,12 @@ function App() {
 				<div>
 					<h2>Counter:</h2>
 					<h1>{count}</h1>
-					<button onClick={onClickMinus} className='Minus'>- Minus</button>
-					<button onClick={onClickPlus} className='Plus'>+ Plus</button>
+					<button onClick={onClickMinus} className='minus'>- Minus</button>
+					<button onClick={onClickPlus} className='plus'>Plus +</button>
 				</div>
 			</div>
 		</>
   );
-}
+};
 
 export default App;
